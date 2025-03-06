c68773657107b881018d6753745b86e27f3b9145
Troy <supavit@g.ucla.edu>
2025-03-06 14:24:48 -0800

    track

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function showScreen(mainId) {
     });
     document.getElementById(mainId).style.display = 'flex';
 }
+
+function showDiv(mainId) {
+    document.getElementById(mainId).style.display = 'flex';
+}
